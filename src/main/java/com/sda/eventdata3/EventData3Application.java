262fd3e54f460427191d348602cf68bd84c775ed
Kedlawv <waldek.bus@gmail.com>
2019-11-24 13:46:01 +0100

    Fresh Spring Boot | Web | Data Swagger Up and Checked H2 Up and Checked Maven Verify Passed

---
+++ src/main/java/com/sda/eventdata3/EventData3Application.java
@@ -0,0 +1,13 @@
+package com.sda.eventdata3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventData3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventData3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/eventdata3/EventData3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.eventdata3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventData3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
